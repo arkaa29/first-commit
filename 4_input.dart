a17a068d249e48dcdab02f9f5cddacb3c85698eb
arkaa29 <arka.imahendra@gmail.com>
2025-09-18 14:23:59 +0700

    belajar input

--- 4_input.dart
+++ 4_input.dart
@@ -1,13 +1,13 @@
-import 'dart:io'; //ini dinamakan package / libary
+//import 'dart:io'; //ini dinamakan package / libary
 
-void main() {
+//void main() {
   /**
    * Perbedaan print dan stdout.write
    * print => otomatis menambahkan baris baru
    * stdout.write => tidak menambahkan baris baru
    */
-  stdout.write("Masukkan nama anda : ");
-  String firstName = stdin.readLineSync()!;
+  //tdout.write("Masukkan nama anda : ");
+  //String firstName = stdin.readLineSync()!;
   /**
    * stdin.readLineSync() =>
    * untuk mengambil inputan dari user
@@ -15,15 +15,16 @@ void main() {
    * tanda ! => null safety atau not null
    */
   
-  stdout.write("Masukkan umur anda : ");
-  int age = int.parse(stdin.readLineSync()!);
+  //stdout.write("Masukkan umur anda : ");
+  //int age = int.parse(stdin.readLineSync()!);
  // int age = int.parse(stdin.readLineSync()!);
   //as int dipakai untuk konversi tipe data
   //int.parse(readLineSync()!) juga bisa dipakai
   //untuk konversi tipe data
 
-   print("Hello nama depanku $firstName dan berumur $age tahun");
-}
+  // print("Hello nama depanku $firstName dan berumur $age tahun");
+//}
+
 
 // nama_depan         => String
 // nama_belakang      => String
@@ -35,3 +36,24 @@ void main() {
 // umurku 14 tahun
 // tinggi badanku 160.5 cm
 // berat badanku 50.1 kg
+
+import 'dart:io';
+
+void main() {
+  stdout.write("Masukkan nama depan anda : ");
+  String firstName = stdin.readLineSync()!;
+  stdout.write("Masukkan nama belakang anda : ");
+  String lastname = stdin.readLineSync()!;
+  stdout.write("Masukkan tinggi badan anda : ");
+  double tinggi = double.parse(stdin.readLineSync()!);
+  stdout.write("masukkan umur anda : ");
+  int age = int.parse(stdin.readLineSync()!);
+  stdout.write("masukkan berat badan anda : ");
+  double berat = double.parse(stdin.readLineSync()!);
+
+  print("Hallo nama depanku $firstName");
+  print("Nama belakangku $lastname");
+  print("Tinggi badanku $tinggi");
+  print("Umur ku $age tahun");
+  print("Berat Badanku $berat");
+}
