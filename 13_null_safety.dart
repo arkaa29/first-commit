89b597b18733e928dc67d8eb74d6795087f8823c
arkaa29 <arka.imahendra@gmail.com>
2025-09-24 13:29:34 +0700

    belajar null safety

---
+++ 13_null_safety.dart
@@ -0,0 +1,59 @@
+// void main(List<String> args) {
+//   /** 
+//    * kegunaan null safety yakni apabila valuenya berupa null
+//    * maka tidak akan mengalami error
+//    * ditandai dengan tanda ?
+//    * String => null safety
+//   */
+
+//   String? favoriteFood = null;
+
+//   //error karena tipe parameternya itu null safety 
+//   buyMeal(favoriteFood); // compile error
+// }
+
+// void buyMeal(String favoriteFood) {
+//   print('I bought a $favoriteFood');
+// }
+/**
+ * ada beberapa cara yang bisa kita lakukan.
+ * 1. ubah parameternya agar bisa menerima nilai null
+ * kodenya yang diubah dibagian parameter di function [buyMeal]
+ * menjadi (String? favoriteFood)
+ * 2. lakukan pengecekan null
+ */
+
+/**
+ * kode yang berhasil menggunakan nilai null
+ * null safety dimulai dibawah ini
+ * ================================
+ */
+
+void main() {
+  String? favoriteFood = 'Ayam Geprek';
+
+  buyMeal(favoriteFood);
+}
+
+void buyMeal(String? favoriteFood) {
+ if (favoriteFood == null) {
+   print('Saya membeli roket ayam geprek matah');
+ } else {
+   print('Saya membeli bakso pentol gosong di firja');
+ }
+}
+
+/**
+ * String? favoriteFood = null
+ * Jika variable favoriteFood nilainya null
+ * maka outputnya itu masuk di kondisi if
+ * Saya membeli roket ayam geprek matah
+ * karen perbandingan favoriteFood == null
+ * 
+ * jika variable fovoriteFood tidak null
+ * maka outputnya yang dalam else
+ * Saya membeli bakso pentol gosong di firja
+ * 
+ * karena dilihat dari operator perbandingannya
+ * yakni == null
+ */
