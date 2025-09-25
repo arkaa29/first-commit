7d8d9ebf68c65332408e98a6d6a6b9c822f69569
arkaa29 <arka.imahendra@gmail.com>
2025-09-25 14:10:47 +0700

    belajar if else

---
+++ 14_if_else.dart
@@ -0,0 +1,50 @@
+void main(List<String> args) {
+  /**
+   * if else
+   * jika suatu kode memiliki kondisi yang terpenehui
+   * maka di eksekusi di kondisi yang pertama yakni if
+   * bila tidak terppenuhi maka lanjut
+   * ke kondisi selanjutnya yaitu else
+   */
+
+  var nilaiUjian = 80;
+  var capek = true;
+  var jamBuka = 7;   // jam buka
+  var jamTutup = 16; // jam tutup toko
+  var jamSekarang = 24; // jam sekarang (saat ini)
+
+  /// tanda ! itu kebalikan (not)
+  /// jika !true => false
+  /// jika !false => true
+  
+  if (capek != true) {
+    print('silahkan istirahat');
+  } else {
+    print('engga capek, gass lanjut dicoding');
+  }
+
+// 7, 8, 16
+if (jamSekarang < jamBuka) {  // 24 < 7 // kurang dari jam
+  print('Toko masih tutup');
+} else if (jamSekarang == jamBuka >= 7 && jamTutup <= 16) { // 24 == 7 && 16 //lewat jam
+  print('Toko Buka');
+} else if (jamSekarang > 16) { // 24 >= 16 //lebih dari jam
+  print('Toko Sudah Tutup');
+} else {
+  print('Toko tutup permanen');
+}
+
+  /**
+   * Nilai E = <=70  // kurang dari sama dengan 70 nilai (E)
+   * Nilai D = 71-15  // 71-75 nilai (D)
+   * Nilai C = 76-85  // 76-85 nilai (C)
+   * Niali B = 86-90  // 86-90 nilai (B)
+   * Nilai A = 91-100 // 91-100 nilai (A)
+   */
+
+  if (nilaiUjian >= 80) {
+    print('Selamat anda lulus ujian');
+  } else {
+    print('Anda akan melakukan remedial');
+  }
+}
