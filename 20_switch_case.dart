2b0f189bef721726825b1e5aa4eaf46a10616022
arkaa29 <arka.imahendra@gmail.com>
2025-10-01 13:53:53 +0700

    belajar switch case

--- 20_switch_case.dart
+++ 20_switch_case.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(List<String> args) {
   /**
    * kode switch case bisa berjalan ketika kondisinya di panggil
@@ -34,4 +36,108 @@ void main(List<String> args) {
     default:
       print('Operator tidak valid');  // 
   }
+
+  /**
+   * buatkan perhitungan dengan inputan 
+   * angkaPertama, angkaKedua, simbolnya
+   * +, -, /, *, %, ~/
+   * 
+   * buatkan pengecekan hari dari senin - jumat itu
+   * "ini adalah hari kerja"
+   * hari dari sabtu - ahad itu
+   * "ini adalah hari libur"
+   * tidak sensitive case (kalau sensitive betul2 sama (identik))
+   * SENIN - SENIN (Sensitive case)
+   * Senin - SENIN (tidak sensitive case)
+   * 
+   * buatkan pengecekan kendaraan
+   * 1. Mobil
+   * 2. Motor
+   * 3. Kapal
+   * 4. Tidak terdefinisi
+   */
+
+// chalange PERTAMAAA
+
+  stdout.write("masukkan angka pertama : ");
+  var pertama = num.parse(stdin.readLineSync()!);
+
+  stdout.write("masukkan angka kedua : ");
+  var kedua = num.parse(stdin.readLineSync()!);
+  
+  stdout.write("masukkan simpol operasi (+, -, /, *, %, ~/); ");
+  var simbolnya = stdin.readLineSync();
+
+  var hasil;
+
+  switch (simbolnya) {
+    case '+':
+      hasil = pertama + kedua;
+      break;
+    case '-':
+      hasil = pertama - kedua;
+      break;
+    case '/':
+      hasil = pertama / kedua;
+      break;
+    case '*':
+      hasil = pertama * kedua;
+      break;
+    case '%':
+      hasil = pertama % kedua;
+      break;
+    case '~/':
+      hasil = pertama ~/ kedua;
+      break;
+    default:
+
+  }
+  print("Hasil : $pertama $simbolnya $kedua = $hasil");
+
+
+// chalange KEDUUAAA
+
+stdout.write("Masukkan nama hari : ");
+var hari = stdin.readLineSync()!;
+
+var hariuper = hari.toUpperCase();
+
+
+switch (hariuper) {
+  case 'SENIN':
+    print('ini hari kerja');
+  case 'SELASA':
+    print('ini hari kerja');
+  case 'RABU':
+    print('ini hari kerja');
+  case 'KAMIS':
+    print('ini hari kerja');
+  case 'JUMAT':
+    print('ini hari kerja');
+  case 'SABTU':
+    print('ini hari libur');
+  case 'MINGGU':
+    print('ini hari libur');
+    break;
+  default:
+  print('apasih yang bener ngapa?!?!?');
+}
+
+
+// chalange KETIGAAAA
+
+stdout.write("Masukkan Kendaraan mu king : ");
+int kendaraan = int.parse(stdin.readLineSync()!);
+
+switch (kendaraan) {
+  case '1':
+  print('mobil');
+  case '2':
+  print('motor');
+  case '3':
+  print('kapal');
+    break;
+  default:
+  print('kendaraan apa kocag?');
+}
 }
