41c64e1819f0763b644fa7a1f8771e324b7dd6cc
arkaa29 <arka.imahendra@gmail.com>
2025-10-06 13:56:12 +0700

    chalaange set

--- 24_set.dart
+++ 24_set.dart
@@ -32,4 +32,45 @@ void main(List<String> args) {
    * intersection (∩) => hanya menggabungkan "data yang sama"
    * dari kedua himpunan yang dipertimbangkan
    */
+  
+  Set<int> segmentA = {1, 3, 5, 7, 9};
+  Set<int> segmentB = {2, 4, 6, 8, 10};
+/**
+ * Union => gabungan dari 2 set / lebih
+ */
+  var unionSegmen = segmentA.union(segmentB);
+  print(unionSegmen);
+
+  var intersectionSegmen = segmentA.intersection(segmentB);
+  print(intersectionSegmen);
+
+  Set<int> segmentC = {1, 5, 7, 9, 10}; //segmentA = {1, 3, 5, 7, 9};
+  var cekSama = segmentA.intersection(segmentC);
+  print(cekSama);
+
+  /**
+   * buatkan 2 set yang menampung nama-nama minimal 7 nama
+   * dari masing-masing set
+   * 1. tampilkan unionnya
+   * 2. tampilkan untersectionnya
+   * 3. tentunkan panjang element dari keduanya
+   * 4. tentunkan panjang element dari unionnya
+   * 5. tentunkan panjang element dari intersectionnya
+   */
+
+  Set<String> set1 = {"Arka", "Bblek", "keni", "Putra", "Hapis", "Sugeng", "Gilang"};
+  Set<String> set2 = {"Jasmin", "lynn", "Aqila", "Reni", "Putri", "kirani", "Lea"};
+
+  Set<String> unionSet = set1.union(set2);
+  print("Union: $unionSet");
+
+  Set<String> intersectionSet = set1.intersection(set2);
+  print("Intersection: $intersectionSet");
+
+  print({set1.length});
+  print({set2.length});
+
+  print({unionSet.length});
+
+  print({intersectionSet.length});
 }
